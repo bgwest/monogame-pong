7199c17260e858c8d6882586b0a2ab28d283a8cb
Benjamin West <23172276+bgwest@users.noreply.github.com>
2020-09-13 19:21:12 -0700

    render ball to the center of the screen as starting pos

--- m8-games/monogame-pong/Game1.cs
+++ m8-games/monogame-pong/Game1.cs
@@ -7,6 +7,8 @@ namespace monogame_pong
     public class Game1 : Game
     {
         Texture2D ballTexture;
+        Vector2 ballPosition;
+        float ballSpeed;
 
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
@@ -20,7 +22,8 @@ namespace monogame_pong
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2);
+            ballSpeed = 100f;
 
             base.Initialize();
         }
@@ -47,7 +50,7 @@ namespace monogame_pong
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             _spriteBatch.Begin();
-            _spriteBatch.Draw(ballTexture, new Vector2(0, 0), Color.White);
+            _spriteBatch.Draw(ballTexture, ballPosition, null, Color.White, 0f, new Vector2(ballTexture.Width / 2, ballTexture.Height / 2), Vector2.One, SpriteEffects.None, 0f);
             _spriteBatch.End();
 
             base.Draw(gameTime);
